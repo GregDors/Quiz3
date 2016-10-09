cae122037cdc565bd908b5dca2792fabd3ea4cfe
gregdorsey <gregdorsey@roaming-171-25.host.udel.edu>
2016-10-09 15:10:10 -0400

    Initial Checkin

---
+++ Quiz-3/src/main/java/Triangle.java
@@ -0,0 +1,64 @@
+import java.util.Date;
+
+public class Triangle extends GeometricObject {
+	private double side1 = 1.0;
+	private double side2 = 1.0;
+	private double side3 = 1.0;
+	private Date datecreated;
+
+	public Triangle() {
+		datecreated = new Date();
+		Triangle t = new Triangle(side1, side2, side3);
+	}
+
+	public Triangle(double side1, double side2, double side3) {
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+		datecreated = new Date();
+	}
+
+	public double getSide1() {
+		return side1;
+	}
+
+	public double getSide2() {
+		return side2;
+	}
+
+	public double getSide3() {
+		return side3;
+	}
+
+	public Date getDatecreated() {
+		return datecreated;
+	}
+
+	@Override
+	/**
+	 * getArea - Finds the area of a triangle you create
+	 * 
+	 * @return
+	 */
+	public double getArea() {
+		double x = (side1 + side2 + side3)/2;
+		return Math.sqrt(x * (x - side1) * (x - side2) * (x - side3));
+	}
+ 
+	@Override
+	/**
+	 * getPerimeter - Finds the perimeter of a triangle you create
+	 * 
+	 * @return
+	 */ 
+	public double getPerimeter() {
+
+		return (side1 + side2 + side3);
+	}
+
+	@Override
+	public String toString() {
+		return ("This is a triangle created on " + datecreated + ". The Area of the triangle is " + getArea()
+				+ "and the perimeter is " + getPerimeter() + ".");
+	}
+}
---
+++ Quiz-3/src/test/java/TestArea.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestArea {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void AreaTesting() {
+		
+		Triangle a = new Triangle(5,12,13);
+		assertTrue(a.getArea() == 30.0);
+		
+	}	
+
+}
